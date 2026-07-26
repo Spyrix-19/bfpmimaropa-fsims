7e1ba7f68456f753e4aaa54b5d9afde729940749
jomarbenito14 <jomarbenito14@gmail.com>
2026-07-26 19:35:10 +0800

    ttuyy

---
+++ src/services/revisionrequestAPI.ts
@@ -0,0 +1,37 @@
+import { apiPost, apiGet, apiDelete, NO_RETRY, GET_RETRY, MUTATION_RETRY_LIGHT } from "@/lib/api";
+import { FSISEditRequestDetailModel, FSISEditRequestDetailParams, FSISEditRequestDTO, 
+    FSISEditRequestModel, FSISEditRequestDeleteParams, FSISEditRequestLedgerParams} from "@/types/revisionrequestType";
+
+
+
+export const revisionrequestAPI = {
+  async create(params: FSISEditRequestDTO) {
+    return await apiPost("/api/v1/FSISEditRequest/Create", params, { ...NO_RETRY });
+  },  
+
+  async getDetail(params?: FSISEditRequestDetailParams, options?: import("@/lib/api").ApiOptions) {
+    return await apiGet<FSISEditRequestDetailModel>("/api/v1/FSISEditRequest/Detail", {
+      params,
+      ...GET_RETRY,
+      ...options,
+    });
+  },
+
+  async getLedger(params?: FSISEditRequestLedgerParams, options?: import("@/lib/api").ApiOptions) {
+    return await apiGet<FSISEditRequestModel[]>("/api/v1/FSISEditRequest/Ledger", {
+      params,
+      ...GET_RETRY,
+      ...options,
+    });
+  },
+
+  async delete(params?: FSISEditRequestDeleteParams) {
+    return await apiDelete("/api/v1/FSISEditRequest/Delete", undefined, {
+      params,
+      ...MUTATION_RETRY_LIGHT,
+    });
+  },
+
+
+  
+};
---
+++ src/types/revisionrequestType.ts
@@ -0,0 +1,81 @@
+export interface FSISEditRequestDTO {
+  requestno: string;
+  referencekey: string;
+  stationno: string;
+  reportyear: number;
+  reportmonth: number;
+  requesttype: string;
+  requestremarks: string;
+  statusno: number;
+  requestedby: string;
+}
+
+export interface FSISEditRequestStatusDTO {
+  requestno: string;
+  requesttype: string;
+  remarks: string;
+  statusno: number;
+  taggedby: string;
+}
+
+export interface FSISEditRequestDetailParams {
+  requestno: string;
+}
+
+export interface FSISEditRequestDetailModel {
+  requestno: string;
+  referencekey: string;
+  stationcode: string;
+  stationname: string;
+  provinceno: string;
+  provincename: string;
+  logourl: string;
+  reportyear: number;
+  reportmonth: number;
+  requestedby: string;
+  requestedbyname: string;
+  daterequested: Date;
+  requesttype: string;
+  remarks: string;
+  statusno: number;
+  statuscode: string;
+  statusname: string;
+  approvedby: string;
+  approvedbyname: string;
+  dateapproved: Date;
+  rejectedby: string;
+  rejectedbyname: string;
+  daterejected: Date;
+}
+
+export interface FSISEditRequestLedgerParams {
+  stationno: string;
+  reportyear: number;
+  provinceno: string;
+  pagenumber: number;
+  pagesize: number;
+}
+
+export interface FSISEditRequestModel {
+  requestno: string;
+  referencekey: string;
+  stationcode: string;
+  stationname: string;
+  provinceno: string;
+  provincename: string;
+  logourl: string;
+  reportyear: number;
+  reportmonth: number;
+  requesttype: string;
+  remarks: string;
+  statusno: number;
+  statuscode: string;
+  statusname: string;
+}
+
+
+export interface FSISEditRequestDeleteParams {
+  requestno: string;
+  deletedby: string;
+  roleno: number;
+}
